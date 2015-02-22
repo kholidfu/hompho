74ce7418f6b4a8bf9a13d7bdc23bd5bf8c962957
sopier <sopier@gmail.com>
2015-02-22 10:27:05 +0700

    synced with flask-admin run.py

--- run.py
+++ run.py
@@ -1,43 +1,66 @@
 #!/usr/bin/env python
-# http://blog.yeradis.com/2012/11/standalone-flask-wsgi-running-under.html
+import argparse
 from app import app
-# app.debug = True
-# print 'Twisted on port 5000...'
-# from twisted.internet import reactor
-# from twisted.web.server import Site
-# from twisted.web.wsgi import WSGIResource
 
-# resource = WSGIResource(reactor, reactor.getThreadPool(), app)
-# site = Site(resource)
+"""
+There are 3 python web server here:
+- werkzeug => ./run.py --builtin
+- twisted  => ./run.py --twisted
+- tornado  => ./run.py --tornado
 
-# reactor.listenTCP(5000, site, interface="0.0.0.0")
-# reactor.run()
+help: ./run.py -h
 
-print 'Tornado on port 5000...'
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-from tornado import autoreload
-from tornado.web import FallbackHandler, Application
-# enabling werkzeug debugging
-# from werkzeug.debug import DebuggedApplication
-# app_debug = DebuggedApplication(app, evalex=True)
-## regular non-async tornado settings
-# http_server = HTTPServer(WSGIContainer(app))
-# http_server.listen(5000)
-# ioloop = IOLoop.instance()
-# autoreload.start(ioloop)
-# ioloop.start()
+adapted from here:
+http://blog.yeradis.com/2012/11/standalone-flask-wsgi-running-under.html
+"""
 
-# tornado async settings
-tr = WSGIContainer(app)
-application = Application([
-    (r".*", FallbackHandler, dict(fallback=tr)),
-    ], debug=True)
 
-application.listen(5000)
-ioloop = IOLoop.instance()
-autoreload.start(ioloop)
-ioloop.start()
+def builtin():
+    print "Built in development server..."
+    app.run(debug=True)
 
-# app.run(debug=True)
+
+def tornado():
+    print 'Tornado on port 5000...'
+    from tornado.wsgi import WSGIContainer
+    from tornado.httpserver import HTTPServer
+    from tornado.ioloop import IOLoop
+
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    IOLoop.instance().start()
+
+
+def twisted():
+    print 'Twisted on port 5000...'
+    from twisted.internet import reactor
+    from twisted.web.server import Site
+    from twisted.web.wsgi import WSGIResource
+
+    resource = WSGIResource(reactor, reactor.getThreadPool(), app)
+    site = Site(resource)
+
+    reactor.listenTCP(5000, site, interface="0.0.0.0")
+    reactor.run()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s",
+                        "--server",
+                        nargs="+",
+                        help="choose between builtin/tornado/twisted as argument",)
+    args = parser.parse_args()
+    
+    if not args.server:  # if no arguments
+        builtin()
+    elif "tornado" in args.server:
+        tornado()
+    else:
+        twisted()
+
+    parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
